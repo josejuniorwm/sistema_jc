89cbd26d42e422df7dd032140b41adeaea991a28
Jose <josejuniorwm@gmail.com>
2025-04-28 18:52:51 -0300

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'Olá';
 });
+
+
+Route::get('/sobre-nos', function () {
+    return 'Olá';
+});
+
+Route::get('/contato', function () {
+    return 'Olá';
+});
