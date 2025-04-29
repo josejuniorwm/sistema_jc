fe4abc3a19ef7e1c8aa43e8f747e69aa873a53e0
Jose <josejuniorwm@gmail.com>
2025-04-29 18:38:21 -0300

    c4

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', ['\App\Http\Controllers\PrincialController::class, 'principal']);
+Route::get('/', function (){
+    return 'Olá';
+});
 
 Route::get('/sobre-nos', function () {
     return 'sobre';
